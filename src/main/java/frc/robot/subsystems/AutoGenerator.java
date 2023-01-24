014090f9cb2dbc63c52b15d7bd882b3552db63c5
Team-1493 <acastagna@albany.k12.ny.us>
2023-01-24 14:42:37 -0500

    added to AutoGenerator

--- src/main/java/frc/robot/subsystems/AutoGenerator.java
+++ src/main/java/frc/robot/subsystems/AutoGenerator.java
@@ -28,14 +28,31 @@ import frc.robot.subsystems.SwerveModule;
 
 
 public class AutoGenerator extends SubsystemBase{
-    public PathPlannerTrajectory testPath = PathPlanner.loadPath("test path", new PathConstraints(4, 3));
+    public PathPlannerTrajectory testPath1 = PathPlanner.loadPath("test path", new PathConstraints(4, 3));
+    public PathPlannerTrajectory testPath2 = PathPlanner.loadPath("test path", new PathConstraints(4, 3));
     private HashMap<String, Command> eventMap = new HashMap<>();
     private SwerveDrive sds;
 
+    //This method will be called once during the beginning of autonomous
     public AutoGenerator(SwerveDrive m_sds) {
+        //defining the SwerveDrive used during autonomous as the instance given by the method
         sds = m_sds;
-    }
 
+        //Putting all possible events in the global eventMap
+        //(These print statements are placeholders for the actual commands that will be run)
+        //Example of events that could be used for multiple paths:
+        eventMap.put("intake_on", new PrintCommand("Intake On"));
+        eventMap.put("intake_off", new PrintCommand("Intake Off"));
+        eventMap.put("place_cube_2", new PrintCommand("Cube has been placed!"));
+        eventMap.put("balance_robot", new PrintCommand("Balanced Robot"));
+
+        //Examples of events that could be used for only one path (testPath):
+        eventMap.put("testPath_marker1", new PrintCommand("Passed marker 1"));
+        eventMap.put("testPath_marker1", new PrintCommand("Passed marker 2"));
+
+    }
+    
+    //Builds and returns a PPSwerveControllerCommand for the given path
     public PPSwerveControllerCommand buildSwerveControlCommand(PathPlannerTrajectory retrievedPath) {
         return new PPSwerveControllerCommand(
             retrievedPath, 
@@ -50,25 +67,35 @@ public class AutoGenerator extends SubsystemBase{
         );
     }
 
-    public FollowPathWithEvents followEventsBuilder(PathPlannerTrajectory retrievedPath, HashMap<String, Command> hashEventMap) {
+    //Builds a FollowPathWithEvents using a given PathPlannerTrajectory
+    public FollowPathWithEvents followEventBuilder(PathPlannerTrajectory retrievedPath) {
         // HashMap<String, Command> eventMap = new HashMap<>();
         // eventMap.put("Waypoint 1", new PrintCommand("nice"));
 
         return new FollowPathWithEvents(
             buildSwerveControlCommand(retrievedPath),
             retrievedPath.getMarkers(),
-            hashEventMap
+            eventMap
         );
     } 
 
-    public SequentialCommandGroup autoCommands() {
+    public SequentialCommandGroup autoCommand1() {
         return new SequentialCommandGroup(
-            new InstantCommand( () -> sds.resetOdometry(testPath.getInitialHolonomicPose())),
-            buildSwerveControlCommand(testPath),
-            new InstantCommand( () -> followEventsBuilder(testPath, eventMap)),
+            new InstantCommand( () -> sds.resetOdometry(testPath1.getInitialHolonomicPose())),
+            buildSwerveControlCommand(testPath1),
+            followEventBuilder(testPath1),
             new InstantCommand( () -> sds.allStop())
         );
-        
     }
+
+    public SequentialCommandGroup autoCommand2(){
+        return new SequentialCommandGroup(
+            new InstantCommand( () -> sds.resetOdometry(testPath1.getInitialHolonomicPose())),
+            buildSwerveControlCommand(testPath2),
+            followEventBuilder(testPath2),
+            new InstantCommand( () -> sds.allStop())
+        );
+    }
+
 }
 
